92de532e4aad8d147479847883b9e14f72b2f836
Kevin Rojas Salazar <kevinfisic@gmail.com>
2025-12-02 17:34:52 +0100

    feat: Creada conexion a la base de datos y verificacion de la contrasenya

--- composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/aplicacion/dependientes/cambiaclave/CambiarClaveDependienteCommand.kt
+++ composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/aplicacion/dependientes/cambiaclave/CambiarClaveDependienteCommand.kt
@@ -1,7 +1,5 @@
 package ies.sequeros.com.dam.pmdm.administrador.aplicacion.dependientes.cambiaclave
 
-
-
 data class CambiarClaveDependienteCommand(val name: String,
                                           val password: String,
                                           )
