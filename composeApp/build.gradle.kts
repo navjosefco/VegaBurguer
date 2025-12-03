cf0fbc615314e6e11299a8ba988870df5e09d235
Kevin Rojas Salazar <kevinfisic@gmail.com>
2025-12-03 18:42:28 +0100

    test: Creacion de test para cambio de contrasenya

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -54,6 +54,8 @@ kotlin {
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+            implementation("io.mockk:mockk:1.13.8")
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
         }
         jvmMain.dependencies {
             implementation(compose.desktop.currentOs)
