0e23576ed70859b605eee440c86cbd6551fba5c4
Kevin Rojas Salazar <kevinfisic@gmail.com>
2025-12-04 17:49:27 +0100

    refactor: cambio de orden para mejor entendimiento

--- composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/modelo/IProductoRepositorio.kt
+++ composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/modelo/IProductoRepositorio.kt
@@ -2,11 +2,13 @@ package ies.sequeros.com.dam.pmdm.administrador.modelo
 
 
 interface IProductoRepositorio {
-    
+
+    suspend fun add(producto: Producto): Unit
+    suspend fun delete(producto:Producto): Boolean
+    suspend fun remove(id:String): Boolean
+    suspend fun update(producto: Producto): Boolean
     suspend fun getAll(): List<Producto>
     suspend fun getById(id: String): Producto?
     suspend fun getByCategoria(idCategoria: String): List<Producto>
-    suspend fun add(producto: Producto): Unit
-    suspend fun update(producto: Producto): Boolean
-    suspend fun delete(producto:Producto): Boolean
+
 }
