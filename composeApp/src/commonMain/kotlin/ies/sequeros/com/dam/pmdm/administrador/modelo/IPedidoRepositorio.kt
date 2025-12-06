dc757e0881f7aab59013485cc887547d8d399867
Kevin Rojas Salazar <kevinfisic@gmail.com>
2025-12-06 21:05:42 +0100

    refactor: Ahora las funciones declaran que tiene que devolver

--- composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/modelo/IPedidoRepositorio.kt
+++ composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/modelo/IPedidoRepositorio.kt
@@ -2,10 +2,10 @@ package ies.sequeros.com.dam.pmdm.administrador.modelo
 
 interface IPedidoRepositorio {
 
+    suspend fun add(pedido: Pedido): Unit
+    suspend fun delete(pedido: Pedido): Boolean
+    suspend fun update(pedido: Pedido): Boolean
     suspend fun getAll(): List<Pedido>
     suspend fun getById(id: String): Pedido?
-    suspend fun add(pedido: Pedido)
-    suspend fun update(pedido: Pedido)
-    suspend fun delete(pedido: Pedido)
     suspend fun getByDependiente(dependienteId: String): List<Pedido>
 }
