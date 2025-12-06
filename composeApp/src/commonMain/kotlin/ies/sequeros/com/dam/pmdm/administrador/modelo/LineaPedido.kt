76dadb47881ff048c1a3bd50368610a0f393cda5
Kevin Rojas Salazar <kevinfisic@gmail.com>
2025-12-06 17:15:01 +0100

    feat: Creada infraestructura de linea de pedido

---
+++ composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/modelo/LineaPedido.kt
@@ -0,0 +1,21 @@
+package ies.sequeros.com.dam.pmdm.administrador.modelo
+
+data class LineaPedido(
+
+    val id: String,
+    val pedidoId: String,
+    val productoId: String,
+    val quantity: Int,
+    val unitPrice: Double
+)
+/*
+CREATE TABLE linea_pedido (
+    id VARCHAR(36) PRIMARY KEY,
+    pedido_id VARCHAR(36) NOT NULL,
+    producto_id VARCHAR(36) NOT NULL,
+    quantity INT NOT NULL,
+    unit_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (pedido_id) REFERENCES pedido(id),
+    FOREIGN KEY (producto_id) REFERENCES producto(id)
+);
+*/
